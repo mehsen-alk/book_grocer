11f4b1d17f7de341e4b72eda761cb61c8951c830
mehsen-alk <mehsen2222@gmail.com>
2022-09-04 16:15:42 +0300

    init

---
+++ lib/core/app/app_prefs.dart
@@ -0,0 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppPreferences {
+  final SharedPreferences _sharedPreferences;
+
+  AppPreferences(this._sharedPreferences);
+}
