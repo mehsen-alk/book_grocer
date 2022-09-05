188247d8d750ba2bb0021240f71c06894b96d02c
mehsen-alk <mehsen2222@gmail.com>
2022-09-05 10:07:48 +0300

    init

---
+++ lib/features/auth/presentation/pages/onboarding/on_boarding_event.dart
@@ -0,0 +1,5 @@
+part of 'on_boarding_bloc.dart';
+
+abstract class OnBoardingEvent extends Equatable {
+  const OnBoardingEvent();
+}
---
+++ lib/features/auth/presentation/pages/onboarding/on_boarding_state.dart
@@ -0,0 +1,10 @@
+part of 'on_boarding_bloc.dart';
+
+abstract class OnBoardingState extends Equatable {
+  const OnBoardingState();
+}
+
+class OnBoardingInitial extends OnBoardingState {
+  @override
+  List<Object> get props => [];
+}
